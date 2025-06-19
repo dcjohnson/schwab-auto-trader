34011aea5115c3928b43e014f447e03981b1432b
Dylan Johnson <dylanatasmsa@gmail.com>
2025-06-19 16:04:15 -0700

    initial commit for building an oauth workflow
    initial test of the server
    
    cert
    
    parse url
    
    implemented it
    
    update
    
    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
-fn main() {
-    println!("Hello, world!");
+use oauth2::basic::BasicClient;
+use oauth2::reqwest;
+
+use oauth2::{
+    AuthUrl, /* AuthorizationCode, */ ClientId, ClientSecret, CsrfToken, PkceCodeChallenge,
+    RedirectUrl, Scope, /* TokenResponse, */ TokenUrl,
+};
+
+use schwab_auto_trader::oauth::token_server;
+// use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    //token_server::run_server(1337).await?;
+
+    let mut tm = std::sync::Arc::new(std::sync::Mutex::new(token_server::TokenManager::new()));
+    let f = tokio::spawn(token_server::run_server(8182, tm));
+
+    // Create an OAuth2 client by specifying the client ID, client secret, authorization URL and
+    // token URL.
+    let client = BasicClient::new(ClientId::new(
+        "".to_string(),
+    ))
+    .set_client_secret(ClientSecret::new("".to_string()))
+    .set_auth_uri(AuthUrl::new(
+        "https://api.schwabapi.com/v1/oauth/authorize".to_string(),
+    )?)
+    .set_token_uri(TokenUrl::new(
+        "https://api.schwabapi.com/v1/oauth/token".to_string(),
+    )?)
+    .set_redirect_uri(RedirectUrl::new("https://127.0.0.1:8182".to_string())?);
+
+    // Generate a PKCE challenge.
+    let (_pkce_challenge, _pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+
+    // Generate the full authorization URL.
+    let (auth_url, csrf_token) = client
+        .authorize_url(CsrfToken::new_random)
+        // Set the desired scopes.
+        .add_scope(Scope::new("readonly".to_string()))
+        // .add_scope(Scope::new("write".to_string()))
+        // Set the PKCE code challenge.
+        // .set_pkce_challenge(pkce_challenge)
+        .url();
+
+    println!("Token: {}", csrf_token.secret());
+    // This is the URL you should redirect the user to, in order to trigger the authorization
+    // process.
+    println!("Browse to: {}", auth_url);
+
+    /*
+        // Once the user has been redirected to the redirect URL, you'll have access to the
+        // authorization code. For security reasons, your code should verify that the `state`
+        // parameter returned by the server matches `csrf_token`.
+
+        let http_client = reqwest::ClientBuilder::new()
+            // Following redirects opens the client up to SSRF vulnerabilities.
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .expect("Client should build");
+
+        // Now you can trade it for an access token.
+        let token_result = client
+            .exchange_code(AuthorizationCode::new(
+                "some authorization code".to_string(),
+            ))
+            // Set the PKCE code verifier.
+           //  .set_pkce_verifier(pkce_verifier)
+            .request_async(&http_client)
+            .await?;
+    */
+    // Unwrapping token_result will either produce a Token or a RequestTokenError.
+
+    f.await??;
+
+    Ok(())
 }
---
+++ src/oauth/token_server.rs
@@ -0,0 +1,204 @@
+// example url it has to serve:
+// https://127.0.0.1:8182/?code=C0.b2F1dGgyLmNkYy5zY2h3YWI0hDl_kgCx2E_Fbt2Htffe9Q%40&session=9f1fcfe6-85da-fd0ae5c2f1c1&state=b9X-RjePuMGyhE
+// https://127.0.0.1:8182/ params: code= , session= , state=
+// What we need is the code. That is the authorization code.
+
+//! Simple HTTPS echo service based on hyper_util and rustls
+//!
+//! First parameter is the mandatory port to use.
+//! Certificate and private key are hardcoded to sample files.
+//! hyper will automatically use HTTP/2 if a client starts talking HTTP/2,
+//! otherwise HTTP/1.1 will be used.
+
+use std::collections::HashMap;
+use std::net::{Ipv4Addr, SocketAddr};
+use std::sync::Arc;
+use std::{fs, io, ops::Deref};
+
+use http_body_util::{/* BodyExt, */ Full};
+use hyper::body::{Bytes, Incoming};
+use hyper::http::{Method, Request, Response, StatusCode};
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use hyper_util::server::conn::auto::Builder;
+use rustls::ServerConfig;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
+use tokio::net::TcpListener;
+use tokio::sync::mpsc;
+use tokio_rustls::TlsAcceptor;
+use url::Url;
+
+fn error(err: String) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, err)
+}
+
+pub struct TokenManager {
+    active_requests: HashMap<String, mpsc::UnboundedSender<String>>,
+}
+
+impl TokenManager {
+    pub fn new() -> Self {
+        Self {
+            active_requests: HashMap::new(),
+        }
+    }
+
+    pub fn send_token(
+        &mut self,
+        auth_token: String,
+        state_token: &String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(s) = self.active_requests.remove(state_token) {
+            s.send(auth_token)?;
+        }
+        Ok(())
+    }
+
+    pub fn new_token_request(&mut self, state_token: String) -> mpsc::UnboundedReceiver<String> {
+        let (s, r) = mpsc::unbounded_channel();
+        self.active_requests.insert(state_token, s);
+        r
+    }
+}
+
+pub async fn run_server(
+    port: u16,
+    tm: std::sync::Arc<std::sync::Mutex<TokenManager>>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Set a process wide default crypto provider.
+    let _ = rustls::crypto::ring::default_provider().install_default();
+    let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
+
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port);
+
+    // Load public certificate.
+    let certs = load_certs("test/cert/cert.pem")?;
+    // Load private key.
+    let key = load_private_key("test/cert/key.pem")?;
+
+    println!("Starting to serve on https://{}", addr);
+
+    // Create a TCP listener via tokio.
+    let incoming = TcpListener::bind(&addr).await?;
+
+    // Build TLS configuration.
+    let mut server_config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .map_err(|e| error(e.to_string()))?;
+    server_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
+    let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
+
+    loop {
+        let (tcp_stream, _remote_addr) = incoming.accept().await?;
+
+        let tls_acceptor = tls_acceptor.clone();
+        let tm_clone = tm.clone();
+        tokio::spawn(async move {
+            let tls_stream = match tls_acceptor.accept(tcp_stream).await {
+                Ok(tls_stream) => tls_stream,
+                Err(err) => {
+                    eprintln!("failed to perform tls handshake: {err:#}");
+                    return;
+                }
+            };
+            if let Err(err) = Builder::new(TokioExecutor::new())
+                .serve_connection(TokioIo::new(tls_stream), Svc::new(tm_clone))
+                .await
+            {
+                eprintln!("failed to serve connection: {err:#}");
+            }
+        });
+    }
+}
+
+struct Svc {
+    tm: std::sync::Arc<std::sync::Mutex<TokenManager>>,
+}
+
+impl Svc {
+    pub fn new(tm: std::sync::Arc<std::sync::Mutex<TokenManager>>) -> Self {
+        Self { tm }
+    }
+}
+
+impl hyper::service::Service<Request<Incoming>> for Svc {
+    type Response = Response<Full<Bytes>>;
+    type Error = hyper::Error;
+    type Future = std::pin::Pin<
+        Box<dyn std::future::Future<Output = Result<Self::Response, Self::Error>> + Send>,
+    >;
+
+    fn call(&self, req: Request<Incoming>) -> Self::Future {
+        let mut response = Response::new(Full::default());
+        match (req.method(), req.uri().path()) {
+            // Help route.
+            (&Method::GET, "/") => {
+                let mut code = None;
+                let mut session = None;
+                let mut state = None; // state must match the csrf_token
+
+                if let Ok(qp) = Url::parse(&req.uri().to_string()) {
+                    for (key, value) in qp.query_pairs() {
+                        match key.deref() {
+                            "code" => code = Some(value.to_string()),
+                            "session" => session = Some(value.to_string()),
+                            "state" => state = Some(value.to_string()),
+                            &_ => (),
+                        }
+                    }
+
+                    if let (Some(code_p), Some(session_p), Some(state_p)) = (code, session, state) {
+                        if let Err(e) = self.tm.lock().unwrap().send_token(code_p.clone(), &state_p)
+                        {
+                            // handle the error somehow
+                        }
+
+                        *response.body_mut() = Full::from(format!(
+                            "code: '{}', session: '{}', state: '{}'",
+                            code_p, session_p, state_p
+                        ));
+                    }
+                }
+            }
+            /*
+            // Echo service route.
+            (&Method::POST, "/echo") => {
+                *response.body_mut() = Full::from(
+                    req.into_body()
+                        .collect()
+                        .await?
+                        .to_bytes(),
+                );
+            }
+            */
+            // Catch-all 404.
+            _ => {
+                *response.status_mut() = StatusCode::NOT_FOUND;
+            }
+        };
+
+        Box::pin(async { Ok(response) })
+    }
+}
+
+// Load public certificate from file.
+fn load_certs(filename: &str) -> io::Result<Vec<CertificateDer<'static>>> {
+    // Open certificate file.
+    let certfile = fs::File::open(filename)
+        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let mut reader = io::BufReader::new(certfile);
+
+    // Load and return certificate.
+    rustls_pemfile::certs(&mut reader).collect()
+}
+
+// Load private key from file.
+fn load_private_key(filename: &str) -> io::Result<PrivateKeyDer<'static>> {
+    // Open keyfile.
+    let keyfile = fs::File::open(filename)
+        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let mut reader = io::BufReader::new(keyfile);
+
+    // Load and return a single private key.
+    rustls_pemfile::private_key(&mut reader).map(|key| key.unwrap())
+}
