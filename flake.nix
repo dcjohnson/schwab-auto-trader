b910fcd443c8c4ba9cb39b7ff19c16bf8ddd3477
Dylan Johnson <dylanatasmsa@gmail.com>
2025-09-07 18:06:41 -0700

    format and shear

--- flake.nix
+++ flake.nix
@@ -5,18 +5,35 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      naersk,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
+        formatter = pkgs.nixfmt-tree;
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              cargo-shear
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
       }
     );
 }
