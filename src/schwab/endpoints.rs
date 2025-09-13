a7bbd1800368d3cc10f6140f105c8d75bdc415f3
Dylan Johnson <dylanatasmsa@gmail.com>
2025-09-13 11:13:58 -0700

    upate

--- src/schwab/endpoints.rs
+++ src/schwab/endpoints.rs
@@ -2,7 +2,7 @@ const MARKET_DATA_ENDPOINT: &str = "https://api.schwabapi.com/marketdata/v1";
 const TRADER_ENDPOINT: &str = "https://api.schwabapi.com/trader/v1";
 
 pub fn accounts() -> String {
-    format!("{}/{}", TRADER_ENDPOINT, "accounts")
+    format!("{}/{}?fields=positions", TRADER_ENDPOINT, "accounts")
 }
 
 pub fn ticker_quotes_data(ticker: &str) -> String {
