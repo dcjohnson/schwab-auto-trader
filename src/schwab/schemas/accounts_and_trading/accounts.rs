71cf60b7a7570a67c4a194f74509fba739a4acdb
Dylan Johnson <dylanatasmsa@gmail.com>
2025-10-08 08:10:15 -0700

    more types

--- src/schwab/schemas/accounts_and_trading/accounts.rs
+++ src/schwab/schemas/accounts_and_trading/accounts.rs
@@ -194,7 +194,30 @@ pub struct Transaction {
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct UserDetails {}
+pub struct UserDetails {
+cd_domain_id	: String,
+login	: String,
+r#type	: UserDetailsType,
+user_id : i64,
+system_user_name	: String,
+first_name	: String,
+last_name	: String,
+broker_rep_code	: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub enum UserDetailsType {
+#[serde(rename(deserialize = "ADVISOR_USER"))]
+    AdvisorUser,
+#[serde(rename(deserialize = "BROKER_USER"))]
+BrokerUser, 
+#[serde(rename(deserialize = "CLIENT_USER"))]
+ClientUser, 
+#[serde(rename(deserialize = "SYSTEM_USER"))]
+SystemUser, 
+#[serde(rename(deserialize = "UNKNOWN"))]
+Unknown
+}
 
 #[derive(Deserialize, Debug)]
 pub enum TransactionType {
@@ -300,4 +323,259 @@ pub enum TransactionActivityType {
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct TransferItem {}
+pub struct TransferItem {
+    instrument: TransactionInstrument, 
+    amount: f64, 
+    cost: f64, 
+    price: f64, 
+    fee_type: FeeType, 
+    position_effect: PositionEffect, 
+}
+
+
+#[derive(Deserialize, Debug)]
+pub enum FeeType{
+#[serde(rename(deserialize = "COMMISSION"))]
+    Commission,
+#[serde(rename(deserialize = "SEC_FEE"))]
+    SecFee,
+#[serde(rename(deserialize = "STR_FEE"))]
+    StrFee,
+#[serde(rename(deserialize = "R_FEE"))]
+    RFee,
+#[serde(rename(deserialize = "CDSC_FEE"))]
+    CdcscFee,
+#[serde(rename(deserialize = "OPT_REG_FEE"))]
+    OptRegFee,
+#[serde(rename(deserialize = "ADDITIONAL_FEE"))]
+    AdditionalFee,
+#[serde(rename(deserialize = "MISCELLANEOUS_FEE"))]
+    MiscellaneousFee,
+#[serde(rename(deserialize = "FUTURES_EXCHANGE_FEE"))]
+    FuturesExchangeFee, 
+#[serde(rename(deserialize = "LOW_PROCEEDS_COMMISSION"))]
+    LowProceedsCommission, 
+#[serde(rename(deserialize = "BASE_CHARGE"))]
+    BaseCharge,
+#[serde(rename(deserialize = "GENERAL_CHARGE"))]
+    GeneralCharge, 
+#[serde(rename(deserialize = "GST_FEE"))]
+    GstFee, 
+#[serde(rename(deserialize = "TAF_FEE"))]
+    TafFee, 
+#[serde(rename(deserialize = "INDEX_OPTION_FEE"))]
+    IndexOptionFee, 
+#[serde(rename(deserialize = "UNKNOWN"))]
+    Unknown
+}
+
+
+#[derive(Deserialize, Debug)]
+pub enum PositionEffect{
+#[serde(rename(deserialize = "OPENING"))]
+Opening, 
+#[serde(rename(deserialize = "CLOSING"))]
+Closing, 
+#[serde(rename(deserialize = "AUTOMATIC"))]
+Automatic, 
+#[serde(rename(deserialize = "UNKNOWN"))]
+Unknown
+
+}
+
+
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub enum TransactionInstrument {
+/*
+	TransactionInstrument{
+oneOf ->	
+TransactionCashEquivalent{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+type	string
+Enum:
+[ SWEEP_VEHICLE, SAVINGS, MONEY_MARKET_FUND, UNKNOWN ]
+}
+CollectiveInvestment{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+type	string
+Enum:
+[ UNIT_INVESTMENT_TRUST, EXCHANGE_TRADED_FUND, CLOSED_END_FUND, INDEX, UNITS ]
+}
+Currency{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+}
+TransactionEquity{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+type	string
+Enum:
+[ COMMON_STOCK, PREFERRED_STOCK, DEPOSITORY_RECEIPT, PREFERRED_DEPOSITORY_RECEIPT, RESTRICTED_STOCK, COMPONENT_UNIT, RIGHT, WARRANT, CONVERTIBLE_PREFERRED_STOCK, CONVERTIBLE_STOCK, LIMITED_PARTNERSHIP, WHEN_ISSUED, UNKNOWN ]
+}
+TransactionFixedIncome{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+type	string
+Enum:
+[ BOND_UNIT, CERTIFICATE_OF_DEPOSIT, CONVERTIBLE_BOND, COLLATERALIZED_MORTGAGE_OBLIGATION, CORPORATE_BOND, GOVERNMENT_MORTGAGE, GNMA_BONDS, MUNICIPAL_ASSESSMENT_DISTRICT, MUNICIPAL_BOND, OTHER_GOVERNMENT, SHORT_TERM_PAPER, US_TREASURY_BOND, US_TREASURY_BILL, US_TREASURY_NOTE, US_TREASURY_ZERO_COUPON, AGENCY_BOND, WHEN_AS_AND_IF_ISSUED_BOND, ASSET_BACKED_SECURITY, UNKNOWN ]
+maturityDate	string($date-time)
+factor	number($double)
+multiplier	number($double)
+variableRate	number($double)
+}
+Forex{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+type	string
+Enum:
+[ STANDARD, NBBO, UNKNOWN ]
+baseCurrency	Currency{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+}
+counterCurrency	Currency{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+}
+}
+Future{
+activeContract	boolean
+default: false
+type	string
+Enum:
+[ STANDARD, UNKNOWN ]
+expirationDate	string($date-time)
+lastTradingDate	string($date-time)
+firstNoticeDate	string($date-time)
+multiplier	number($double)
+}
+Index{
+activeContract	boolean
+default: false
+type	string
+Enum:
+[ BROAD_BASED, NARROW_BASED, UNKNOWN ]
+}
+TransactionMutualFund{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+fundFamilyName	string
+fundFamilySymbol	string
+fundGroup	string
+type	string
+Enum:
+[ NOT_APPLICABLE, OPEN_END_NON_TAXABLE, OPEN_END_TAXABLE, NO_LOAD_NON_TAXABLE, NO_LOAD_TAXABLE, UNKNOWN ]
+exchangeCutoffTime	string($date-time)
+purchaseCutoffTime	string($date-time)
+redemptionCutoffTime	string($date-time)
+}
+TransactionOption{
+assetType*	string
+Enum:
+Array [ 8 ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+expirationDate	string($date-time)
+optionDeliverables	[
+xml: OrderedMap { "name": "optionDeliverables", "wrapped": true }
+TransactionAPIOptionDeliverable{
+rootSymbol	string
+strikePercent	integer($int64)
+deliverableNumber	integer($int64)
+deliverableUnits	number($double)
+deliverable	{
+}
+assetType	assetTypestring
+Enum:
+[ EQUITY, MUTUAL_FUND, OPTION, FUTURE, FOREX, INDEX, CASH_EQUIVALENT, FIXED_INCOME, PRODUCT, CURRENCY, COLLECTIVE_INVESTMENT ]
+}]
+optionPremiumMultiplier	integer($int64)
+putCall	string
+Enum:
+[ PUT, CALL, UNKNOWN ]
+strikePrice	number($double)
+type	string
+Enum:
+[ VANILLA, BINARY, BARRIER, UNKNOWN ]
+underlyingSymbol	string
+underlyingCusip	string
+deliverable	{
+}
+}
+Product{
+assetType*	string
+Enum:
+[ EQUITY, OPTION, INDEX, MUTUAL_FUND, CASH_EQUIVALENT, FIXED_INCOME, CURRENCY, COLLECTIVE_INVESTMENT ]
+cusip	string
+symbol	string
+description	string
+instrumentId	integer($int64)
+netChange	number($double)
+type	string
+Enum:
+[ TBD, UNKNOWN ]
+}
+}
+*/
+}
