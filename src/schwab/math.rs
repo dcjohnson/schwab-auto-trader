7227d94d248a4638dbeba3a6cf879e2e27cf904d
Dylan Johnson <dylanatasmsa@gmail.com>
2025-11-28 16:14:22 -0800

    update

---
+++ src/schwab/math.rs
@@ -0,0 +1,15 @@
+/* Math to support the investment calculations.
+ * lambda = The amount of new money to invest. 
+ * tau = The current value of the total investments.
+ * x_1 = target investment goal in percent of a given collection. 1 >= x_1 >= 0
+ * x_1_o = current investment value in percent of a given collection 1 >= x_1_o >= 0
+ * x_1 + x_2 + ... x_n = 1 where all values are 1 >= and >= 0. 
+ * x_1_o + x_2_o + ... x_n_o = 1 where all values are 1 >= and >= 0.
+ * theta = lambda + tau
+ * a_1 = x_1 * theta - x_1_o * tau | This value a_1 is the amount we are going to invest for a given
+ * collection. If a_1 >= 0 then that collection will not be invested with that new money. If a_1 >=
+ * lambda then the collection that has the biggest an will recieve the entire amount of new money
+ * to be invested. 
+ * a_1 + a_2 + ... + a_n = lambda
+ * (a_1 + t * x_1_o) + (a_2 + t * x_2_o) + ... + (a_n + t * x_n_o) = theta
+ **/
--- src/schwab/mod.rs
+++ src/schwab/mod.rs
@@ -1,3 +1,4 @@
+pub mod math;
 pub mod account_manager;
 pub mod client;
 pub mod endpoints;
